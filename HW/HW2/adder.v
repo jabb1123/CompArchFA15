bd1bd431e0101b7cb1529d75993e3cf2874412df
Jacob Riedel <jr112394@gmail.com>
2015-09-24 20:24:40 -0400

    Finished all test benches

--- HW/HW2/adder.v
+++ HW/HW2/adder.v
@@ -1,3 +1,8 @@
+// define gates with delays
+`define AND and #50
+`define OR or #50
+`define NOT not #50
+
 module behavioralFullAdder(sum, carryout, a, b, carryin);
 output sum, carryout;
 input a, b, carryin;
@@ -7,15 +12,62 @@ endmodule
 module structuralFullAdder(out, carryout, a, b, carryin);
 output out, carryout;
 input a, b, carryin;
-  // Your adder code here
+
+wire nA;
+wire nB;
+wire nCi;
+
+wire anAanBaCi;
+wire anAaBanCi;
+wire aAanBanCi;
+wire anAaBaCi;
+wire aAanBaCi;
+wire aAaBanCi;
+wire aAaBaCi;
+
+`NOT notA(nA,a);
+`NOT notB(nB,b);
+`NOT notCi(nCi,carryin);
+
+`AND and0(anAanBaCi,nA,nB,carryin);
+`AND and1(aAanBanCi,a,nB,nCi);
+`AND and2(anAaBanCi,nA,b,nCi);
+
+`AND and3(aAaBaCi,a,b,carryin);
+
+`AND and4(aAanBaCi,a,nB,carryin);
+`AND and5(anAaBaCi,nA,b,carryin);
+`AND and6(aAaBanCi,a,b,nCi);
+
+`OR orSum(out,anAanBaCi,aAanBanCi,anAaBanCi,aAaBaCi);
+
+`OR orCo(carryout,aAanBaCi,anAaBaCi,aAaBanCi,aAaBaCi);
+
 endmodule
 
 module testFullAdder;
 reg a, b, carryin;
 wire sum, carryout;
-behavioralFullAdder adder (sum, carryout, a, b, carryin);
+// behavioralFullAdder adder (sum, carryout, a, b, carryin);
+structuralFullAdder adder (sum, carryout, a, b, carryin);
 
 initial begin
-  // Your test code here
+$display("A  B  Ci | Co  O  | Expected Output");
+a=0;b=0;carryin=0; #1000 
+$display("%b  %b  %b  |  %b  %b  | 0  0", a, b, carryin, carryout, sum);
+a=0;b=0;carryin=1; #1000 
+$display("%b  %b  %b  |  %b  %b  | 0  1", a, b, carryin, carryout, sum);
+a=1;b=0;carryin=0; #1000 
+$display("%b  %b  %b  |  %b  %b  | 0  1", a, b, carryin, carryout, sum);
+a=1;b=0;carryin=1; #1000 
+$display("%b  %b  %b  |  %b  %b  | 1  0", a, b, carryin, carryout, sum);
+a=0;b=1;carryin=0; #1000 
+$display("%b  %b  %b  |  %b  %b  | 0  1", a, b, carryin, carryout, sum);
+a=0;b=1;carryin=1; #1000 
+$display("%b  %b  %b  |  %b  %b  | 1  0", a, b, carryin, carryout, sum);
+a=1;b=1;carryin=0; #1000 
+$display("%b  %b  %b  |  %b  %b  | 1  0", a, b, carryin, carryout, sum);
+a=1;b=1;carryin=1; #1000 
+$display("%b  %b  %b  |  %b  %b  | 1  1", a, b, carryin, carryout, sum);
 end
 endmodule
--- HW/HW2/multiplexer.v
+++ HW/HW2/multiplexer.v
@@ -1,3 +1,8 @@
+// define gates with delays
+`define AND and #50
+`define OR or #50
+`define NOT not #50
+
 module behavioralMultiplexer(out, address0,address1, in0,in1,in2,in3);
 output out;
 input address0, address1;
@@ -11,10 +16,51 @@ module structuralMultiplexer(out, address0,address1, in0,in1,in2,in3);
 output out;
 input address0, address1;
 input in0, in1, in2, in3;
-  // Your multiplexer code here
+
+wire nA0wire;
+wire nA1wire;
+wire aI0wire;
+wire a1Iwire;
+wire aI2wire;
+wire aI3wire;
+
+`NOT notA0(nA0wire, address0);
+`NOT notA1(nA1wire, address1);
+
+`AND andI0(aI0wire,in0,nA0wire,nA1wire);
+`AND andI1(aI1wire,in1,address0,nA1wire);
+`AND andI2(aI2wire,in2,nA0wire,address1);
+`AND andI3(aI3wire,in3,address0,address1);
+
+`OR orGate(out,aI0wire,aI1wire,aI2wire,aI3wire);
+ 
 endmodule
 
 
 module testMultiplexer;
-  // Your test code here
+reg addr0, addr1;
+reg in0,in1,in2,in3;
+wire out;
+// behavioralMultiplexer multiplexer (out, addr0,addr1, in0,in1,in2,in3);
+structuralMultiplexer multiplexer (out, addr0,addr1, in0,in1,in2,in3); // Swap after testing
+
+initial begin
+$display("A0 A1| I0 I1 I2 I3 | O | Expected Output");
+addr0=1;addr1=1;in0=1'bX;in1=1'bX;in2=1'bX;in3=0; #1000 
+$display("%b  %b |  %b  %b  %b  %b | %b | 0", addr0, addr1, in0, in1, in2, in3, out);
+addr0=1;addr1=1;in0=1'bX;in1=1'bX;in2=1'bX;in3=1; #1000
+$display("%b  %b |  %b  %b  %b  %b | %b | 1", addr0, addr1, in0, in1, in2, in3, out);
+addr0=0;addr1=1;in0=1'bX;in1=1'bX;in2=0;in3=1'bX; #1000 
+$display("%b  %b |  %b  %b  %b  %b | %b | 0", addr0, addr1, in0, in1, in2, in3, out);
+addr0=0;addr1=1;in0=1'bX;in1=1'bX;in2=1;in3=1'bX; #1000 
+$display("%b  %b |  %b  %b  %b  %b | %b | 1", addr0, addr1, in0, in1, in2, in3, out);
+addr0=1;addr1=0;in0=1'bX;in1=0;in2=1'bX;in3=1'bX; #1000 
+$display("%b  %b |  %b  %b  %b  %b | %b | 0", addr0, addr1, in0, in1, in2, in3, out);
+addr0=1;addr1=0;in0=1'bX;in1=1;in2=1'bX;in3=1'bX; #1000 
+$display("%b  %b |  %b  %b  %b  %b | %b | 1", addr0, addr1, in0, in1, in2, in3, out);
+addr0=0;addr1=0;in0=0;in1=1'bX;in2=1'bX;in3=1'bX; #1000 
+$display("%b  %b |  %b  %b  %b  %b | %b | 0", addr0, addr1, in0, in1, in2, in3, out);
+addr0=0;addr1=0;in0=1;in1=1'bX;in2=1'bX;in3=1'bX; #1000 
+$display("%b  %b |  %b  %b  %b  %b | %b | 1", addr0, addr1, in0, in1, in2, in3, out);
+end
 endmodule
